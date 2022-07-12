ccad569662e720e59b022c08c2ed6365d9a09832
Mile Mijatovic <mile@milemijatovic.com>
2022-07-12 20:33:57 +0200

    Google analytics added

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
